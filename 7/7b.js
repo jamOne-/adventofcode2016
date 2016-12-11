3cb93fcfe1f88b267458f277f626ad9098be0abd
jamOne- <dominik.rabij@gmail.com>
2016-12-11 01:25:02 +0100

    7b, 8

--- 7/7b.js
+++ 7/7b.js
@@ -3,3 +3,39 @@ fs.readFile('input', 'UTF-8', (err, input) => {
     console.log(solve(input));
 });
 
+function solve(input) {
+    let lines = input.split('\n');
+    let count = 0;
+
+    lines.forEach(line => {
+        let abas = {};
+        let babs = {};
+        let inSquares = false;
+
+        for (let i = 0; i < line.length - 2; i++) {
+            let aba = line[i] == line[i + 2] && line[i] != line[i + 1];
+
+            if (line[i] == '[') inSquares = true;
+            else if (line[i] == ']') inSquares = false;
+            else if (!inSquares && aba) {
+                if (babs[line[i + 1] + line[i] + line[i + 1]]) {
+                    count++;
+                    return;
+                }
+
+                abas[line[i] + line[i + 1] + line[i + 2]] = true;
+            }
+
+            else if (inSquares && aba) {
+                if (abas[line[i + 1] + line[i] + line[i + 1]]) {
+                    count++;
+                    return;
+                }
+
+                babs[line[i] + line[i + 1] + line[i + 2]] = true;
+            }
+        }
+    });
+
+    return count;
+}
---
+++ 8/8b.js
@@ -0,0 +1,63 @@
+fs = require('fs');
+fs.readFile('input', 'UTF-8', (err, input) => {
+    solve(input);
+});
+
+const WIDTH = 50;
+const HEIGHT = 6;
+
+function solve(input) {
+    let screen = Array(WIDTH * HEIGHT).fill('.');
+    input.split('\n').forEach(line => loadInstruction(screen, line));
+    
+    for (let i = 0; i < HEIGHT; i++) {
+        let row = '';
+        for (let j = 0; j < WIDTH; j++)
+            row += screen[i * WIDTH + j];
+        
+        console.log(row);
+    }
+}
+
+function loadInstruction(screen, instruction) {
+    let match;
+
+    if (match = instruction.match(/rect (.*)x(.*)/)) {
+        let a = +match[1];
+        let b = +match[2];
+
+        for (let i = 0; i < b; i++)
+            for (let j = 0; j < a; j++)
+                screen[i * WIDTH + j] = '#';
+    }
+
+    else if (match = instruction.match(/rotate column x=(.*) by (.*)/)) {
+        let x = +match[1];
+        let shift = +match[2];
+
+        let column = [];
+        for (let i = 0; i < HEIGHT; i++)
+            column.push(screen[x + i * WIDTH]);
+
+        for (let i = 0; i < shift; i++)
+            column.unshift(column.pop());
+
+        for (let i = 0; i < HEIGHT; i++)
+            screen[x + i * WIDTH] = column[i];
+    }
+
+    else if (match = instruction.match(/rotate row y=(.*) by (.*)/)) {
+        let y = +match[1];
+        let shift = +match[2];
+
+        let row = [];
+        for (let i = 0; i < WIDTH; i++)
+            row.push(screen[y * WIDTH + i]);
+
+        for (let i = 0; i < shift; i++)
+            row.unshift(row.pop());
+
+        for (let i = 0; i < WIDTH; i++)
+            screen[y * WIDTH + i] = row[i];
+    }
+}
